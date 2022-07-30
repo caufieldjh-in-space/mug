9b87ab07f29c43bf35d2639eea6876226ab76404
caufieldjh-in-space <severaltimes [at] gmail.com>
2022-07-30 15:14:18 -0400

    Add script to get resource samples

---
+++ mug/get_resource.py
@@ -0,0 +1,32 @@
+"""Utilities to get text from resources."""
+
+from distutils.command.clean import clean
+import heapq
+from pathlib import Path
+import random
+
+RES_DIR = "resources"
+ITEM_TYPES = {"colors": "colors.tsv"}
+
+def get_items(item_type: str,
+              count: int = 1,
+              ) -> list:
+
+    mod_path = Path(__file__).parent
+    res_file = (mod_path / RES_DIR / ITEM_TYPES[item_type]).resolve()
+
+    with res_file.open() as infile:
+        sample = heapq.nlargest(count, 
+                                infile,
+                                key=lambda L: random.random()
+                                )
+
+    sample = clean_sample(sample)
+
+    return sample
+
+def clean_sample(rawlist: list) -> list:
+
+    cleanlist = [s.strip() for s in rawlist]
+
+    return cleanlist
