f26400f9f1c2583f4c4fdd261302c52a016a39f5
caufieldjh-in-space <severaltimes [at] gmail.com>
2022-08-02 17:23:04 -0400

    More company name

--- mug/generators/company.py
+++ mug/generators/company.py
@@ -1,6 +1,7 @@
 """company.py - generate company names + metadata"""
 
 import random
+import string
 
 from mug.generators.generic import MUGProduct
 from mug.get_resource import get_items
@@ -35,17 +36,28 @@ class Company(MUGProduct):
                       "surname"
                       ]
 
-        tc = random.randint(0,1)
+        tc = random.randint(0,2)
         if tc == 0:
+            name = ""
+            for _ in range(0,random.randint(3,6)):
+                name = name + random.choice(string.ascii_uppercase)
+        elif tc == 1:
             part1 = get_items(random.choice(part_types), 1)[0]["id"].title()
             part2 = get_items(random.choice(part_types), 1)[0]["id"].title()
             name = f"{part1}{part2}"
-        elif tc == 1:
-            parts = get_items("forename", 2)
+        elif tc == 2:
+            parts = get_items("surname", 2)
             part1 = parts[0]["id"].title()
             part2 = parts[1]["id"].title()
             conn = random.choice(["&","And"])
-            name = f"{part1} {conn} {part2}"
+            if random.randint(0,5) > 0:
+                if random.randint(0,1) == 0:
+                    part3 = get_items("surname", 1)[0]["id"].title()
+                    name = f"{part1}, {part2} {conn} {part3}"
+                else:
+                    name = f"{part1}, {part2} {conn} {part2}"
+            else:
+                name = f"{part1} {conn} {part2}"
 
         if random.randint(0,4) > 0:
             name = name.replace(" ", "")
