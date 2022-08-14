2d8f0aa4f6a545194ac7de1df37932176894187f
caufieldjh-in-space <severaltimes [at] gmail.com>
2022-08-14 12:46:45 -0400

    Notes

--- mug/generators/generic.py
+++ mug/generators/generic.py
@@ -5,6 +5,9 @@ import uuid
 class MUGProduct:
     """ Parent class of all MUG generated objects."""
 
+    # TODO: add fantasy place generator
+    # TODO: add tech co generator (as child of Company)
+
     def __init__(self):
         """Init - assign a unique ID"""
         self.id = str(uuid.uuid4())
