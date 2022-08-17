8d6c793a621302879e00db46ea500fe8315e85d7
caufieldjh-in-space <severaltimes [at] gmail.com>
2022-08-17 14:12:39 -0400

    Add name parts

--- mug/generators/person.py
+++ mug/generators/person.py
@@ -12,10 +12,20 @@ class Person(MUGProduct):
         # TODO: make distinction between formal and informal name
 
         super().__init__()
-        self.name = self.make_person_name()
+        (self.name_informal, self.name_formal) = self.make_person_name()
 
-    def make_person_name(self) -> str:
+    def make_person_name(self) -> tuple:
+        """Produce two versions of a person's name.
 
+        The result is a tuple. The first entry
+        is the informal name, e.g.,
+        Craigley Pitts
+        and the second entry is the formal name, e.g.,
+        Prof. Craigley Nortimer Pitts, Esq
+        The level of detail present in the casual name
+        is random - it may be as short as a single name,
+        but will generally include a forename and surname
+        without titles."""
         # TODO: expand based on wastegen namegen
 
         forename = get_items("forename", 1)[0]["id"]
