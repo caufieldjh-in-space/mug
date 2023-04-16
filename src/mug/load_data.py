55a8b6db189fd64272f05b3d5d752c776bd0058a
caufieldjh-in-space <severaltimes [at] gmail.com>
2023-04-16 13:20:10 -0400

    Add sampler function

--- src/mug/load_data.py
+++ src/mug/load_data.py
@@ -1,7 +1,6 @@
 """Load data from TSVs in resources directory."""
 
 import polars as pl
-import pandas as pd
 
 from pathlib import Path
 
@@ -9,10 +8,19 @@ RESOURCE_DIR = Path("src/mug/resources")
 
 
 def load_res(resname: str):
-    """Load TSV and return data frame."""
+    """Load TSV and return full data frame."""
     fn = f"{resname}.tsv"
     respath = RESOURCE_DIR / fn
 
-    res = pl.read_csv(respath, rechunk=False, separator="\t").to_pandas()
+    res = pl.read_csv(respath, rechunk=False, separator="\t")
 
     return res
+
+def sample_res(resname: str):
+    """Retrieve a sample from a resource, returning a dict."""
+
+    res = load_res(resname)
+
+    sample = (res.sample(n=1)).to_dict(as_series=False)
+
+    return sample
