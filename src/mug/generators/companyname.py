ab8ff3661db44acc5668eb3328b6eaf373d6e801
caufieldjh <j.harry.caufield@gmail.com>
2023-05-15 11:36:37 -0400

    Update company name gen

--- src/mug/generators/companyname.py
+++ src/mug/generators/companyname.py
@@ -4,10 +4,10 @@ import sys
 import uuid
 from random import choice, randint
 
-from mug.utils.textgen import make_acronym
-
 from mug.load_class import get_class_details
 from mug.load_data import sample_res
+from mug.utils.textgen import make_acronym
+from mug.utils.vary_text import all_upper, controlled_misspell, uncontrolled_misspell
 
 GEN_NAME = "CompanyName"
 GEN_MOD = sys.modules[__name__]
@@ -37,9 +37,12 @@ def generate():
 
     return contents
 
-
+# TODO: add portmanteaus, though that may mean adding a function
+#       to get lists of many ids at once
+# TODO: include some generic company names like TriState or General
+# TODO: add some more Weirdness
 def company_name_main():
-    tc = randint(0, 1)
+    tc = randint(0, 2)
     if tc == 0:  # Acronym
         name = make_acronym()
     elif tc == 1:  # Simple word or phrase
@@ -52,17 +55,51 @@ def company_name_main():
                     "fictionalbeast",
                     "fictionalcharacter",
                     "givenname",
+                    "latinword",
                 ]
             )
         )["id"][0].title()
+    elif tc == 2:  # Person names
+        name1 = sample_res("familyname")["id"][0]
+        name2 = sample_res("familyname")["id"][0]
+        conn1 = choice(["", " ", ", "])
+        name = f"{name1}{conn1}{name2}"
+        if randint(0, 3) == 0:
+            name3 = sample_res("familyname")["id"][0]
+            conn2 = choice(["", "-", " & ", " + ", " And "])
+            name = f"{name}{conn2}{name3}"
+
+    # Modifiers
+    # TODO: fix up this first modifier, it isn't quite right
+    if randint(0, 6) == 0:
+        postfix = sample_res(choice(["company_postfix_casual", "genericplace"]))["id"][0]
+        conn = choice(["", " "])
+        name = f"{name}{conn}{postfix}"
+    if randint(0, 8) == 0:
+        name = name.replace(" ", "")
+    if randint(0, 6) == 0:
+        name = controlled_misspell(name)
+    if randint(0, 6) == 0:
+        name = uncontrolled_misspell(name)
+    if randint(0, 9) == 0:
+        name = all_upper(name)
+
+
     return [name]
 
-
+# TODO: tune this up a bit as sometimes it generates very strange combos
+#       Maybe use just one business/industry type and just one postfix?
 def company_name_suffix():
     suffixes = []
     used_already = []
-    for i in range(randint(1,2)):
-        this_choice = choice(["company_postfix_formal", "corporate_department"])
+    for i in range(randint(1, 2)):
+        this_choice = choice(
+            [
+                "businesstype",
+                "company_postfix_formal",
+                "industry",
+            ]
+        )
         if this_choice not in used_already:
             suffixes.append(sample_res(this_choice)["id"][0])
         used_already.append(this_choice)
